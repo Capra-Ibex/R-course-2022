4fc5de33e468a6e15ac51349c0c7bff0e76c9977
Pablo <Capra-Ibex>
2022-11-16 15:20:22 +0100

    last lesson

---
+++ 04_Figure_with_ggplot2.R
@@ -0,0 +1,71 @@
+####~~~~~~~~~~~~~Header~~~~~~~~~~~~~~####
+####
+#### TITLE : Learning graphics in R with ggplot2
+####
+#### Auteur(s) : Raguet P.
+####
+#### Date creation : 2022-11-16
+####
+#### Date modification : 2022-11-16
+####
+#### Comments : Original author: Maxwell T.
+####
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+#### WORKING DIRECTORY ####
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+# dir <- "directory"
+# setwd(dir)
+getwd()
+
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+#### PACKAGES ####
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+library(tidyverse)
+# library(ggplot2) # ggplot2 is already in the tidyverse metapackage
+# library(dplyr) # dplyr is already in the tidyverse metapackage
+
+library(datasets)
+library(ggpubr)
+
+library(svglite)
+
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+#### DATA IMPORT AND BUILD ####
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+# clean the environment
+rm(list = ls())
+
+#download the two datasets (available from the package "datasets")
+tab <- as.data.frame(Orange)
+tab2 <- as.data.frame(nhtemp)
+
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+#### Orange tree ####
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+
+
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+#### Temperature data ####
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+
+
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+#### Orange tree ####
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+
+
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+#### TEST ####
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+
+
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+#### TIPS & CLUES ####
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+
+
+
+
+
